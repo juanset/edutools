d7172c7e1a2938fecd9a01817a4210611aa1314a
daespinosag <daespinosag@unal.edu.co>
2016-12-20 15:13:35 -0500

    response in raim

--- app/Http/Controllers/raimController.php
+++ app/Http/Controllers/raimController.php
@@ -6,22 +6,24 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Response;
 
 class raimController extends Controller
 {
     public function session_create(Requests $request)
     {
       # code...
-      return json_encode('Hola Creador');
+      return response()->json($request->all());
+
     }
     public function session_update(Requests $request)
     {
       # code...
-      return json_encode('Hola editor');
+      return response()->json($request->all());
     }
     public function session_delete(Requests $request)
     {
       # code...
-      return json_encode('Hola eliminador');
+      return response()->json($request->all());
     }
 }

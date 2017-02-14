6dbb90ebd6f6a69aeae77f9170231e018136d735
Juanset <jnset@hotmail.com>
2017-02-14 00:37:15 -0500

    Cambio libro
    Cambio en los estilos

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -14,7 +14,7 @@
 
     <body class="cuerpo" >
     <header>
-        <style>a{color:#dd4f24}.contenedor{height:0;overflow:hidden;padding-bottom:56.25%;padding-top:30px;position:relative}.contenedor.iframe,nav:before{left:0;position:absolute;top:0}.contenedor.iframe{height:100%;width:100%}header,nav{position:relative}.cuerpo{background:#222d32;color:#444;font-family:tahoma;font-size:16px}footer{background:#d8e0e2;color:#a6b4cd;text-align:center;height:150px;width:100%}.logo{border-right:1px solid #dd4f24;border-radius:2px;float:left;width:50px}footer h3{font-size:14px;font-weight:400}footer h3 strong{display:block}footer h3 .mejor:after,footer h3 .mejor:before{content:" - "}header{background:#E8E8E8;box-shadow:0 5px rgba(0,0,0,.2);color:#222D32;overflow:hidden;padding:.5em;z-index:1}header .titular{margin:0 65px 0 70px}header .titular .titulo{font-size:22px;font-weight:400;margin:0;padding:0}nav{background:#22bce0;border-left:15px solid #fff}nav:before{border-bottom:20px solid transparent;border-left:7px solid #fff;border-top:20px solid transparent;content:""}nav .menu{height:40px;line-height:20px;margin:0;overflow-x:auto;padding:0;white-space:nowrap}nav .menu li{display:inline-block}nav .menu li:hover{background:#dd4f24}nav .menu li a{color:#fff;display:block;margin:.3em;padding:.2em;text-decoration:none}.principal{padding:.5em}#flipbook .page{background:#efebe9}#flipbook .subt{color:#19db37}#flipbook p{color:#000;text-align:justify;margin:.5em}#flipbook img{width:250px;height:250px;align-items:center;border-radius:4%}#flipbook .hard{background:#02204e;color:#fff;text-align:center}#flipbook .title{color:#e6dcdc}</style>
+        <style>a{color:#dd4f24}.contenedor{height:0;overflow:hidden;padding-bottom:56.25%;padding-top:30px;position:relative}.contenedor.iframe,nav:before{left:0;position:absolute;top:0}.contenedor.iframe{height:100%;width:100%}header,nav{position:relative}.cuerpo{background:#222d32;color:#444;font-family:tahoma;font-size:16px}footer{background:#d8e0e2;color:#a6b4cd;text-align:center;height:150px;width:100%}.logo{border-right:1px solid #dd4f24;border-radius:2px;float:left;width:50px}footer h3{font-size:14px;font-weight:400}footer h3 strong{display:block}footer h3 .mejor:after,footer h3 .mejor:before{content:" - "}header{background:#E8E8E8;box-shadow:0 5px rgba(0,0,0,.2);color:#222D32;overflow:hidden;padding:.5em;z-index:1}header .titular{margin:0 65px 0 70px}header .titular .titulo{font-size:22px;font-weight:400;margin:0;padding:0}nav{background:#22bce0;border-left:15px solid #fff}nav:before{border-bottom:20px solid transparent;border-left:7px solid #fff;border-top:20px solid transparent;content:""}nav .menu{height:40px;line-height:20px;margin:0;overflow-x:auto;padding:0;white-space:nowrap}nav .menu li{display:inline-block}nav .menu li:hover{background:#dd4f24}nav .menu li a{color:#fff;display:block;margin:.3em;padding:.2em;text-decoration:none}.principal{padding:.5em}#flipbook .page{background:#efebe9}#flipbook .page .subt{color:#08357b}#flipbook .page .title{color:#05204a}#flipbook .subt{color:#e2f4ba}#flipbook p{color:#000;text-align:justify;margin:.5em}#flipbook img{width:250px;height:250px;align-items:center;border-radius:5px}#flipbook .hard{background:#02204e;color:#fff;text-align:center}#flipbook .hard .title{color:#e6dcdc}</style>
     </header>
     <section class="principal">
 
@@ -69,6 +69,7 @@
             </div>
             <div class="btn-group">
                 <button type="submit" class="btn btn-success" value="Guardar" id="boton01">Guardar</button>
+
             </div>
             {!! Form::close() !!}
         </div>
@@ -183,6 +184,7 @@
                     alert(result);
                 });
             });
+
 
         });
     </script>
